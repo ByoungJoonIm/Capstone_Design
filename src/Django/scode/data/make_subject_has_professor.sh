6841060d200a84419cb0f9383c8faf1a0aded477
BJ-Lim <fortress0427@google.com>
2019-05-18 18:30:26 +0900

    revised subject_has ...

--- src/Django/scode/data/make_subject_has_professor.sh
+++ src/Django/scode/data/make_subject_has_professor.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-for((i=1;i<9;i++));do
+for((i=1;i<30;i++));do
 	echo "- model: 'judge.subject_has_professor'
   fields:
-    sub_seq: $(expr $i % 3 + 1)
-    professor_id: '0000$(expr $i % 4 + 1)'
+    sub_seq: $(expr $i % 7 + 1)
+    professor_id: '0000$(expr $i % 5 + 1)'
     represent_yn: true
-    input_id: 'shp_20190507$i'
-    input_ip: '127.0.0.$i'
-    input_date: 2019-05-07 01:00:0$i
-    update_id: '20190507$i'
-    update_ip: '127.0.0.$i'
-    update_date: 2019-05-07 01:00:0$i"
+    input_id: 'shp_20190507$(expr $i % 10)'
+    input_ip: '127.0.0.$(expr $i % 10)'
+    input_date: 2019-05-07 01:00:0$(expr $i % 10)
+    update_id: '20190507$(expr $i % 10)'
+    update_ip: '127.0.0.$(expr $i % 10)'
+    update_date: 2019-05-07 01:00:0$(expr $i % 10)"
 done
