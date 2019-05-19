a1c92e47e9c9a01a0b1a1ad9a3dff7400a47cb5e
BJ-Lim <fortress0427@gmail.com>
2019-05-19 21:12:55 +0900

    Now we can see different result by selecting different value

--- src/Django/scode/judge/urls.py
+++ src/Django/scode/judge/urls.py
@@ -8,23 +8,22 @@ urlpatterns = [
     url(r'^professor$', ProfessorMainLV.as_view(), name='professor'),
 
     # example : /professor/python_01
-    url(r'^professor/subject$', ProfessorSubjectLV.as_view(), name='subject'),
+    url(r'^professor/(?P<subject_title>[a-zA-Z0-9]+)_(?P<classes>[0-9][0-9])$', ProfessorSubjectLV.as_view(), name='subject'),
 
     # example : /professor/python_01/add
-    url(r'^professor/subject/add$', ProfessorCreateView.as_view(), name='add'),
+    url(r'^professor/(?P<subject_title>[a-zA-Z0-9]+)_(?P<classes>[0-9][0-9])/add$', ProfessorCreateView.as_view(), name='add'),
 
     # example : /professor/python_01/update
-    url(r'^professor/subject/update$', ProfessorUpdateView.as_view(), name='update'),
+    url(r'^professor/(?P<subject_title>[a-zA-Z0-9]+)_(?P<classes>[0-9][0-9])/update$', ProfessorUpdateView.as_view(), name='update'),
 
     # example : /professor/python_01/delete
-    url(r'^professor/subject/delete$', ProfessorDeleteView.as_view(), name='delete'),
+    url(r'^professor/(?P<subject_title>[a-zA-Z0-9]+)_(?P<classes>[0-9][0-9])/delete$', ProfessorDeleteView.as_view(), name='delete'),
 
     # example : /professor/python_01/settings
-    url(r'^professor/subject/settings$', ProfessorSettingsView.as_view(), name='settings'),
+    url(r'^professor/(?P<subject_title>[a-zA-Z0-9]+)_(?P<classes>[0-9][0-9])/settings$', ProfessorSettingsView.as_view(), name='settings'),
 
     # example : /professor/python_01/3
-    url(r'^professor/subject/result$', ProfessorResultLV.as_view(), name='result'),
-
+    url(r'^professor/(?P<subject_title>[a-zA-Z0-9]+)_(?P<classes>[0-9][0-9])/(?P<sequence>[0-9]+)$', ProfessorResultLV.as_view(), name='result'),
 
 
     # example : /student
--- src/Django/scode/judge/views.py
+++ src/Django/scode/judge/views.py
@@ -8,8 +8,16 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from judge.models import *
 from django.views.generic.base import TemplateView
+#from .forms import PostForm
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
+from django.db import connection
 
 # Create your views here.
+
 
 #-- Here is test views
 class ProfessorCreateView(TemplateView):
@@ -27,12 +35,13 @@ class ProfessorSettingsView(TemplateView):
 
 #-- Here is developing area
 class ProfessorMainLV(ListView):
-    # We need to revise professor_id to parameter.
+    professor_id = '00001'
     sql = 'SELECT judge_professor.professor_id,title,classes \
-        FROM judge_professor , judge_subject_has_professor, judge_subject \
-        WHERE judge_professor.professor_id=judge_subject_has_professor.professor_id \
-        AND judge_subject.pri_key=judge_subject_has_professor.sub_seq_id \
-        AND judge_professor.professor_id="00001";'
+           FROM judge_professor , judge_subject_has_professor, judge_subject \
+           WHERE judge_professor.professor_id=judge_subject_has_professor.professor_id \
+           AND judge_subject.pri_key=judge_subject_has_professor.sub_seq_id \
+           AND judge_professor.professor_id=' + professor_id + ' \
+           ORDER BY judge_subject.title;'
 
     #queryset = professor.objects.all()
     queryset = professor.objects.raw(sql)
@@ -40,29 +49,45 @@ class ProfessorMainLV(ListView):
     context_object_name = "objects"
 
 class ProfessorSubjectLV(ListView):
-    # We need to revise professor_id, title, classes to parameter.
-    sql = 'SELECT judge_professor.professor_id, title , judge_assignment.sub_seq_id as sub_seq_id, sequence, assignment_name ,assignment_desc \
-        FROM judge_subject_has_professor, judge_professor, judge_assignment, judge_subject \
-        WHERE judge_subject_has_professor.sub_seq_id = judge_assignment.sub_seq_id \
-        AND judge_professor.professor_id = judge_subject_has_professor.professor_id \
-        AND judge_subject.pri_key = judge_subject_has_professor.sub_seq_id \
-        AND judge_subject.title = "subject2" \
-        AND judge_subject.classes = "01" \
-        AND judge_professor.professor_id="00001";'
+    #It doesn't used.
+    queryset = professor.objects.all()
 
-    queryset = professor.objects.raw(sql)
+#    context_object_name = "objects"
     template_name = 'judge/professor/professor_subject_list.html'
-    context_object_name = "objects"
+    paginate_by = 10
 
+    def post(self, request, *args, **kwargs):
+        form = request.POST
+        title = form.get('title')
+        classes = form.get('classes')
+        professor_id = '00001'
+        sql = 'SELECT *\
+         FROM judge_subject_has_professor, judge_professor, judge_assignment, judge_subject \
+         WHERE judge_subject_has_professor.sub_seq_id = judge_assignment.sub_seq_id \
+         AND judge_professor.professor_id = judge_subject_has_professor.professor_id \
+         AND judge_subject.pri_key = judge_subject_has_professor.sub_seq_id \
+         AND judge_subject.title = "' + title + '" \
+         AND judge_subject.classes = "' + classes + '" \
+         AND judge_professor.professor_id = "' + professor_id + '";'
+
+        self.queryset = professor.objects.raw(sql)
+
+        kwargs = {
+            'title': title,
+            'classes': classes
+        }
+
+        return render(request, self.template_name, {'objects': self.queryset, 'form': kwargs})
 
 class ProfessorResultLV(ListView):
     # We need to revise sub_seq_id
+    sub_seq_id = 2
     sql = 'SELECT judge_student.student_id,judge_student.student_name,score \
         FROM judge_student,judge_submit,judge_assignment \
         WHERE judge_assignment.sub_seq_id = judge_submit.sub_seq_id \
         AND judge_student.student_id = judge_submit.student_id \
         AND judge_assignment.sequence = judge_submit.sequence_id \
-        AND judge_submit.sub_seq_id = 2;'
+        AND judge_submit.sub_seq_id = ' + str(sub_seq_id) + ';'
 
     queryset = student.objects.raw(sql)
     template_name = 'judge/professor/professor_result_list.html'
