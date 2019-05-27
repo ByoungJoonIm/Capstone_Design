85c00d3effb437dd9c5712489970a4a23735adc6
BJ-Lim <fortress0427@gmail.com>
2019-05-27 21:14:50 +0900

    Changed datas for logic

--- src/Django/scode/data/make_subject.sh
+++ src/Django/scode/data/make_subject.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-for((i=1;i<9;i++));do
+for((i=0;i<30;i++));do
 	echo "- model: judge.subject
   fields:
-    year: '201$i'
+    year: '201$(expr $i % 10)'
     semester: $(expr $i % 2 + 1)
     subject_cd: '12345$i'
     classes: '0$(expr $i % 2 + 1)'
-    title: 'subject$i'
-    grade: $i
+    title: 'subject$(expr $i + 1)'
+    grade: $(expr $i % 4 + 1)
     input_id: 'sub_20190507$i'
     input_ip: '127.0.0.$i'
-    input_date: 2019-05-07 00:00:0$i
+    input_date: 2019-05-07 00:00:0$(expr $i % 10)
     update_id: 'sub_20190507$i'
     update_ip: '127.0.0.$i'
-    update_date: 2019-05-07 00:00:0$i"
+    update_date: 2019-05-07 00:00:0$(expr $i % 10)"
 done
