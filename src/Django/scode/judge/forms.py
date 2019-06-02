4ab04c0cdf5f047013e31a0d75d882d01bf98516
BJ-Lim <fortress0427@gmail.com>
2019-06-03 02:05:42 +0900

    added deadline field in judge_assignment

--- src/Django/scode/judge/forms.py
+++ src/Django/scode/judge/forms.py
@@ -3,6 +3,7 @@ from django import forms
 class AssignmentForm(forms.Form):
     assignment_name = forms.CharField(label="assignment_name")
     assignment_desc = forms.CharField(label="assignment_desc", widget=forms.Textarea)
+    deadline = forms.IntegerField(label="deadline", initial=7)
     in_file = forms.FileField(label="in_file")
     out_file = forms.FileField(label="out_file")
 
