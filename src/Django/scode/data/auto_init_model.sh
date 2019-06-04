2222dd329c7b505b905f0a79a71f2da7d67f0890
BJ-Lim <fortress0427@gmail.com>
2019-06-04 21:54:18 +0900

    Removed data dir! We replace it to test dir.

--- src/Django/scode/data/auto_init_model.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mysql < recreate_db
-cd ..
-rm judge/migrations/000* > /dev/null 2>&1
-python manage.py migrate
-python manage.py makemigrations
-python manage.py migrate judge
-#mysql < sqlchecker
